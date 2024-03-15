872fea18c546f1e35e83092aaf5cd6b679e09d8b
LasseThomsenSkole <lath0003@stud.kea.dk>
2024-03-15 11:58:44 +0100

    added SQL Scripts

---
+++ SqlScripts/TuristGuideDB.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS Turist_Guide;
+USE Turist_Guide;
+CREATE TABLE attractions (
+	id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    description varchar(255)NOT NULL DEFAULT ' ',
+    city varchar(20) NOT NULL DEFAULT ' ');
+
+CREATE TABLE attraction_tag (
+	attactions_id int ,
+    tag_id int);
+    
+CREATE TABLE tags (
+	id int AUTO_INCREMENT PRIMARY KEY,
+    name varchar(30) NOT NULL);
