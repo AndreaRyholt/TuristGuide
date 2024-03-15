02348a9d5a227806fc2152e7c75fdae1f693e7ee
hantamil <mira0004@stud.kea.dk>
2024-03-15 11:08:28 +0100

    docs mappe

--- src/test/java/tourism/TuristGuideApplicationTests.java
+++ src/test/java/tourism/TuristGuideApplicationTests.java
@@ -1,13 +1,23 @@
 package tourism;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tourism.controller.TouristController;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class TuristGuideApplicationTests {
 
+
+
+	@Autowired //skal der Autowired før alle klasserne, eller kan man skrive dem alle herunder?
+	private TouristController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
